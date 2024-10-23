3a430580b54ecfa2f8ecdaa92589128bddf5db65
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-23 17:52:06 +0200

    Implement add and remove

--- lab4/script.js
+++ lab4/script.js
@@ -39,12 +39,18 @@ const getStylesForElement = (element) => {
 
 // task2
 
+let lastImageIndex = 0;
+
+const container = () => document.getElementById('image-container');
+
+const lastImage = () => document.getElementById(`image-${lastImageIndex}`)
+
 const add = () => {
-    
+    container().innerHTML += `<img id="image-${++lastImageIndex}" alt="Kramatorsk" src="https://upload.wikimedia.org/wikipedia/commons/c/c5/%D0%9F%D0%B0%D0%BB%D0%B0%D1%86_%D0%BA%D1%83%D0%BB%D1%8C%D1%82%D1%83%D1%80%D0%B8_%D1%82%D0%B0_%D1%82%D0%B5%D1%85%D0%BD%D1%96%D0%BA%D0%B8%2C_%D0%9A%D1%80%D0%B0%D0%BC%D0%B0%D1%82%D0%BE%D1%80%D1%81%D1%8C%D0%BA_DJI_0002.jpg" width="800">`;
 };
 
 const zoomIn = () => {
-
+    
 };
 
 const zoomOut = () => {
@@ -52,5 +58,9 @@ const zoomOut = () => {
 };
 
 const remove = () => {
-
+    const image = lastImage();
+    if (image) {
+        image.remove();
+        lastImageIndex--;
+    }
 };
