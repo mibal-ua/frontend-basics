b6e15872867628ba13c5e086ee77e4345cb456c0
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-23 17:18:30 +0200

    Implement task1

--- lab4/script.js
+++ lab4/script.js
@@ -1,13 +1,36 @@
 'use strict';
 
+const stylesForState = {
+    0: {
+        backgroundColor: 'blue',
+        color: 'white',
+    },
+    1: {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+    },
+};
+
+const state = {
+    first: -1,
+    second: -1,
+};
+
 const toggleFirst = () => {
     const first = document.getElementById('first');
-    first.style.backgroundColor = 'blue';
-    first.style.color = 'white';
+    const style = getStylesForElement('first');
+    Object.assign(first.style, style);
 };
 
 const toggleSecond = () => {
     const second = document.querySelector('#second');
-    second.style.backgroundColor = 'lightgreen';
-    second.style.color = 'black';
+    const style = getStylesForElement('second');
+    Object.assign(second.style, style);
+};
+
+const getStylesForElement = (element) => {
+    const currentState = state[element];
+    const styles = stylesForState[currentState];
+    state[element] = (currentState + 1) % 2;
+    return styles;
 };
