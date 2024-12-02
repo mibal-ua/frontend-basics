e558930e6aee24cf06a92468e42592a408406034
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-02 09:16:41 +0100

    Implement card update logic

--- lab6/script.js
+++ lab6/script.js
@@ -4,6 +4,7 @@ function download() {
     fetchUsers()
         .then(users => {
             console.log('users', users);
+            return users;
         })
         .then(users => updateCards(users));
 }
@@ -22,6 +23,26 @@ function fetchUsers() {
 }
 
 function updateCards(users) {
-    //todo
-    return undefined;
+    const container = document.getElementById('cards-container');
+    container.innerHTML = '';
+    for (let user of users) {
+        const cardHtml = getCardHtml(user);
+        container.innerHTML += cardHtml;
+    }
+}
+
+function getCardHtml({ cell, picture, email, location }) {
+    const photo = picture.large;
+    const { coordinates, city } = location;
+    return (
+        `<div class="card">
+            <img alt="User photo" src="${photo}">
+            <div class="info">
+                <p class="field"><span>Cell:</span> ${cell}</p>
+                <p class="field"><span>City:</span> ${city}</p>
+                <p class="field"><span>E-mail:</span> ${email}</p>
+                <p class="field"><span>Coordinates:</span> lt: ${coordinates.latitude}; lg: ${coordinates.longitude}</p>
+            </div>
+        </div>`
+    );
 }
