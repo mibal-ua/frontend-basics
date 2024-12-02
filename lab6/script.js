435472d772ff5e1a9259ee7a0c192d1e3aab424d
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-02 08:58:51 +0100

    Implement fetching logic

--- lab6/script.js
+++ lab6/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
 function download() {
-    console.log('Donwloading...');
+    fetchUsers()
+        .then(users => {
+            console.log('users', users);
+        })
+        .then(users => updateCards(users));
+}
+
+function fetchUsers() {
+    return fetch('https://randomuser.me/api?results=4')
+        .then((response) => response.json())
+        .then((response) => response.results)
+        .then((json) => {
+            console.log('json=', json);
+            return json;
+        })
+        .catch(err => {
+            console.error('An error occurred when fetching users', err)
+        });
+}
+
+function updateCards(users) {
+    //todo
+    return undefined;
 }
