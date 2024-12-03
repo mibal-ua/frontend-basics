93631e397dcb058f3ce0d35825a89b2510c6e413
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-03 18:57:25 +0100

    Design app structure

---
+++ lab7/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import Header from "./app/Header";
+import Content from "./app/Content";
+import Image from "./app/Image";
+
+function App() {
+    return (<>
+        <Header/>
+        <Content/>
+        <Image/>
+    </>);
+}
+
+export default App;
---
+++ lab7/src/app/Image.jsx
@@ -0,0 +1,7 @@
+import {Component} from "react";
+
+export default class Image extends Component {
+    render() {
+        return null;
+    }
+}
