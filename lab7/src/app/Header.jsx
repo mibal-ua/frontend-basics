38667d96a0e050e5c8a1d929b6cb75d45e6671c8
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-03 19:06:11 +0100

    Implement `Header`

--- lab7/src/app/Header.jsx
+++ lab7/src/app/Header.jsx
@@ -1,3 +1,34 @@
+import React from "react";
+
+const states = {
+    NONE: {}, 
+    BLUE: { backgroundColor: 'blue', color: 'white' },
+    GREEN: { backgroundColor: 'lightgreen', color: 'black' },
+}
+
 export default function Header() {
-    return null;
+    const [firstState, setFirstState] = React.useState(states.NONE);
+    const [secondState, setSecondState] = React.useState(states.NONE);
+
+    const toggleFirst = () => {
+        if (firstState === states.BLUE) {
+            setFirstState(states.GREEN);
+        } else {
+            setFirstState(states.BLUE);
+        }
+    };
+
+    const toggleSecond = () => {
+        if (secondState === states.BLUE) {
+            setSecondState(states.GREEN);
+        } else {
+            setSecondState(states.BLUE);
+        }
+    };
+
+    return (<>
+        <h2 style={firstState} onClick={toggleFirst}>Балахон Михайло Олексійович</h2>
+        <p style={secondState} onClick={toggleSecond}>Народився 18 вересня 2004 року в місті Краматорськ</p>
+        <p>Отримав повну загальну середню освіту в ЗОШ 26. Студент НТУУ "КПІ" 3 курсу</p>
+    </>);
 }
